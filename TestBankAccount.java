65f8aef6c99f48898f6338284b8dc0fc7172988b
axnasim <nasim0807@gmail.com>
2019-01-24 13:55:10 -0600

    Update TestBankAccount.java

--- TestBankAccount.java
+++ TestBankAccount.java
@@ -10,4 +10,12 @@ public class TestBankAccount {
     double amount = account.debit(5);
     Assert.assertEquals(5.0, amount);
   }
+ 
+ public class TestBankAccount {
+  public void testDebitWithInSufficientFunds() {
+    BankAccount account = new BankAccount(10);
+    double amount = account.debit(11);
+    Assert.assertEquals(10.0, amount);
+  } 
+  
 }
